213835f46ba6a4b691a92f51d11a56d8f1520cb3
Aleksey-Shutov <124181930+Aleksey-Shutov@users.noreply.github.com>
2023-02-02 19:49:48 +0300

    Add files via upload

---
+++ menu.inc.php
@@ -0,0 +1,10 @@
+ <nav>
+                <ul>
+                    <li><a href="#"> Учеба </a></li>
+                    <li><a href="#"> Порфолио </a></li>
+                    <li><a href="#"> Статьи </a></li>
+                    <li><a href="#"> Ссылки </a></li>
+                    <li><a href="#"> Контакты </a></li>
+					<li><a href="https://skillfactory.ru/" target=_blank> SkillFactory </a></li>
+                </ul>    
+            </nav>
